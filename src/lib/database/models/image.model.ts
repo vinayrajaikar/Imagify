c35319cf029c8d3446c84a5816e9a6b9c2834384
vinayrajaikar <vinayrajaikar@gmail.com>
2025-03-06 14:55:48 +0000

    fix: fixed TypeScript and ESLint issues

--- src/lib/database/models/image.model.ts
+++ src/lib/database/models/image.model.ts
@@ -4,41 +4,41 @@ export interface IImage extends Document {
   title: string;
   transformationType: string;
   publicId: string;
-  secureURL: string; 
+  secureURL: string;
   width?: number;
   height?: number;
-  config?: object; 
-  transformationUrl?: string; 
+  config?: object;
+  transformationUrl?: string;
   aspectRatio?: string;
   color?: string;
   prompt?: string;
-  author: {
+  author: Schema.Types.ObjectId | {
     _id: string;
     firstName: string;
     lastName: string;
-  }
+  };
   createdAt?: Date;
   updatedAt?: Date;
 }
 
 const ImageSchema = new Schema(
-    {
-        title: { type: String, required: true },
-        transformationType: { type: String, required: true },
-        publicId: { type: String, required: true },
-        secureURL: { type: String, required: true },
-        width: { type: Number },
-        height: { type: Number },
-        config: { type: Object },
-        transformationUrl: { type: String },
-        aspectRatio: { type: String },
-        color: { type: String },
-        prompt: { type: String },
-        author: { type: Schema.Types.ObjectId, ref: 'User' }
-    },
-    {
-        timestamps:true
-    }
+  {
+    title: { type: String, required: true },
+    transformationType: { type: String, required: true },
+    publicId: { type: String, required: true },
+    secureURL: { type: String, required: true },
+    width: { type: Number },
+    height: { type: Number },
+    config: { type: Object },
+    transformationUrl: { type: String },
+    aspectRatio: { type: String },
+    color: { type: String },
+    prompt: { type: String },
+    author: { type: Schema.Types.ObjectId, ref: 'User' }
+  },
+  {
+    timestamps: true
+  }
 );
 
 const Image = models?.Image || model('Image', ImageSchema);
