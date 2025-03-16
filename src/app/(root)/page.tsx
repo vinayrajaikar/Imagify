d03d5dc90d11e201cb8ec2bc55deb882070bcb4c
vinayrajaikar <vinayrajaikar@gmail.com>
2025-03-16 06:56:06 +0000

    feat: added stripe

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,45 @@
+import { Collection } from "@/components/shared/Collection"
+import { navLinks } from "@/constants"
+import { getAllImages } from "@/lib/actions/image.actions"
+import Image from "next/image"
+import Link from "next/link"
 
-const Home = () => {
+const Home = async ({searchParams}: SearchParamProps) => {
+  const page = Number(searchParams?.page) || 1;
+  const searchQuery = (searchParams?.query as string) || '';
+
+  const images = await getAllImages({ page, searchQuery })
+  
   return (
-    <div>
-      <div>Home Page</div>
-    </div>
+    <>
+      <section className="sm:flex justify-center items-center hidden h-72 flex-col gap-4 rounded-[20px] border bg-banner bg-cover bg-no-repeat p-10 bg-[#7068fc] shadow-inner">
+        <h1 className="text-[36px] font-semibold sm:text-[44px] leading-[120%] sm:leading-[56px] max-w-[500px] flex-wrap text-center text-white text-4xl font-bold">
+          Unlease Your Create Vision with Imagify
+        </h1>
+        <ul className="flex justify-center items-center w-full gap-20">
+          {navLinks.slice(1,5).map((link)=>(
+            <Link 
+              key={link.route} 
+              href={link.route}
+              className="flex justify-center items-center flex-col gap-2">
+                <li className="flex justify-center items-center w-fit rounded-full bg-white p-4">
+                  <Image src={link.icon} alt="image" width={24} height={24} />
+                </li>
+                <p className="p-14-medium text-center text-white">{link.label}</p>
+            </Link>
+          ))}
+        </ul>
+      </section>
+
+      <section className="sm:mt-12">
+          <Collection 
+            hasSearch={true}
+            images={images?.data}
+            totalPages={images?.totalPage}
+            page={page}
+          />
+      </section>
+    </>
   )
 }
 
