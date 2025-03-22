01a38e11535c9abb8caaafe3eb3668aab27ca35c
vinayrajaikar <vinayrajaikar@gmail.com>
2025-03-22 09:33:49 +0000

    feat: implemented stripe

--- src/app/(root)/profile/page.tsx
+++ src/app/(root)/profile/page.tsx
@@ -1,11 +1,67 @@
-import React from 'react'
+import { auth } from "@clerk/nextjs/server";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 
-const Profilepage = () => {
+import { Collection } from "@/components/shared/Collection";
+import Header from "@/components/shared/Header";
+import { getUserImages } from "@/lib/actions/image.actions";
+import { getUserById } from "@/lib/actions/user.action";
+
+export type ParamsType = Promise<{ searchParams: { page?: string } }>;
+
+const Profile = async (props: { params: ParamsType }) => {
+  const { searchParams } = await props.params;
+  const page = Number(searchParams?.page) || 1;
+  const { userId } =await auth();
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
+  const images = await getUserImages({ page, userId: user._id });
+
   return (
-    <div>
-      Profilepage
-    </div>
-  )
-}
+    <>
+      <Header title="Profile" />
 
-export default Profilepage
+      <section className="flex flex-col gap-5 sm:flex-row md:mt-8 md:gap-10e">
+        <div className="w-full rounded-[16px] border-2 border-purple-200/20 bg-white p-5 shadow-lg shadow-purple-200/10 md:px-6 md:py-8">
+          <p className="p-14-medium md:p-16-medium">CREDITS AVAILABLE</p>
+          <div className="mt-4 flex items-center gap-4">
+            <Image
+              src="/assets/icons/coins.svg"
+              alt="coins"
+              width={50}
+              height={50}
+              className="size-9 md:size-12"
+            />
+            <h2 className="h2-bold text-dark-600">{user.creditBalance}</h2>
+          </div>
+        </div>
+
+        <div className="w-full rounded-[16px] border-2 border-purple-200/20 bg-white p-5 shadow-lg shadow-purple-200/10 md:px-6 md:py-8">
+          <p className="p-14-medium md:p-16-medium">IMAGE MANIPULATION DONE</p>
+          <div className="mt-4 flex items-center gap-4">
+            <Image
+              src="/assets/icons/photo.svg"
+              alt="coins"
+              width={50}
+              height={50}
+              className="size-9 md:size-12"
+            />
+            <h2 className="h2-bold text-dark-600">{images?.data.length}</h2>
+          </div>
+        </div>
+      </section>
+
+      <section className="mt-8 md:mt-14">
+        <Collection
+          images={images?.data}
+          totalPages={images?.totalPages}
+          page={page}
+        />
+      </section>
+    </>
+  );
+};
+
+export default Profile;
